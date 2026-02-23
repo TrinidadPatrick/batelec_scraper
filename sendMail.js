7f04f872bcf06c18b9bb1c413562a92e68cbd4a9
TrinidadPatrick <trinidadpatrick019@gmail.com>
2026-02-23 19:06:50 +0800

    Changed port and added secure to true on nodemailer setup options

--- sendMail.js
+++ sendMail.js
@@ -1,16 +1,20 @@
 const nodemailer = require('nodemailer')
 
+const ENVIRONMENT = process.env.ENVIRONMENT
+
 module.exports.sendMail = async (body) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
-        }
+        },
+        port: 465,
+        secure: ENVIRONMENT === 'LOCAL' ? false : true
       });
 
       const mailOptions = {
-        from: 'swaply.support@gmail.com',
+        from: 'app.mailer@gmail.com',
         to: 'trinidadpatrick019@gmail.com',
         subject: 'Batelec Advisory',
         html: `<main>${body}<main>`
